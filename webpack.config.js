c86b3c30a79601671967bde801f4abe55ef94f01
t-ueno <t.ueno.brook@gmail.com>
2019-09-12 18:16:44 +0900

    書式フォーマットのみ

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,43 @@
-
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const webpackMerge = require('webpack-merge');
 
 var baseConfig = {
-  resolve: {
-    extensions: ['.js', '.ts'],
-  },
-  module: {
-    rules: [{
-      test: /\.ts$/,
-      use: "ts-loader"
-    }]
-  },
-  mode: process.env.NODE_ENV || "development",
-  devServer: {
-    contentBase: __dirname + '/public',
-    publicPath: '/',
-    noInfo: true,
-    historyApiFallback: true
+	resolve: {
+		extensions: ['.js', '.ts'],
+	},
+	module: {
+		rules: [{
+			test: /\.ts$/,
+			use: "ts-loader"
+		}]
+	},
+	mode: process.env.NODE_ENV || "development",
+	devServer: {
+		contentBase: __dirname + '/public',
+		publicPath: '/',
+		noInfo: true,
+		historyApiFallback: true
 
-  },
-  plugins: [new HTMLWebpackPlugin({
-    template: "./src/index.html"
-  })]
+	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: "./src/index.html"
+		})
+	]
 }
 
 var GeoLocoMapConfig = webpackMerge(baseConfig, {
-  output: {
-    filename: 'bundle.js',
-    path: __dirname + "/dist",
-    libraryExport: 'GeoLocoMap',
-    library:  'GeoLocoMap',
-    libraryTarget: 'umd'
-  },
-  entry: './src/GeoLoco.ts',
-  name: 'umd',
-  mode: 'production',
+	output: {
+		filename: 'bundle.js',
+		path: __dirname + "/dist",
+		libraryExport: 'GeoLocoMap',
+		library: 'GeoLocoMap',
+		libraryTarget: 'umd'
+	},
+	entry: './src/GeoLoco.ts',
+	name: 'umd',
+	mode: 'production',
 })
-
-
 
 
 module.exports = GeoLocoMapConfig;
