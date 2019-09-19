a3a0e364eca986fac69950c4a244c24b05965cbb
t-ueno <t.ueno.brook@gmail.com>
2019-09-19 17:49:04 +0900

    不要なファイルを削除

--- src/usecases/RequestUseCase.ts
+++
@@ -1,9 +0,0 @@
-import { ILatLng } from "../entities/LatLng"
-import { ApiRequest } from "../entities/ApiRequest"
-
-export  class RequestUseCase {
-
-    constructor(coordinate: ILatLng, url: string, zoom?: number) {
-        return new ApiRequest(coordinate,url,zoom)
-    }
-}
