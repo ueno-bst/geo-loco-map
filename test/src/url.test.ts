7c8b4ff4284e0a5c4a3ba381550ae19132bac5f5
t-ueno <t.ueno.brook@gmail.com>
2019-09-26 13:56:51 +0900

    テスト環境調整中

---
+++ test/src/url.test.ts
@@ -0,0 +1,11 @@
+// import {describe, it} from "mocha";
+import {URLBuilder} from "../../src/utils/URLBuilder";
+
+describe("AAA", () => {
+    it("AAAA", () => {
+        const i = new URLBuilder("http://test/test.jp");
+        console.info("AAA", i.build());
+        // assert.equal("TEST", "TEST");
+    });
+});
+
