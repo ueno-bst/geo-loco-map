d6374662ede986132fbf6c576a241eb989a8e02c
t-ueno <led.mypace@gmail.com>
2020-01-13 02:00:14 +0900

    コンパイル設定の変更

--- webpack.config.js
+++ webpack.config.js
@@ -6,57 +6,58 @@ const DtsBundlePlugin = require('dts-bundle-webpack');
 const packageJson = require("./package");
 const version = packageJson.version;
 
-
-module.exports = {
-	entry: './src/GeoLocoMap.ts',
-	name: 'umd',
-	resolve: {
-		extensions: ['.ts'],
-	},
-	output: {
-		filename: `geo-loco-map.js`,
-		path: __dirname + "/dist/" + version,
-		libraryExport: 'GeoLocoMap',
-		library: 'GeoLocoMap',
-		libraryTarget: 'umd2'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.ts(x?)$/,
-				use: "ts-loader"
-			},
-			{
-				test: /\.html$/,
-				use: {
-					loader: 'html-loader',
+module.exports = env => {
+	return {
+		entry: './src/GeoLocoMap.ts',
+		name: 'umd',
+		resolve: {
+			extensions: ['.ts'],
+		},
+		output: {
+			filename: `geo-loco-map.js`,
+			path: __dirname + "/dist/" + version,
+			libraryExport: 'GeoLocoMap',
+			library: 'GeoLocoMap',
+			libraryTarget: 'umd2'
+		},
+		module: {
+			rules: [
+				{
+					test: /\.ts(x?)$/,
+					use: "ts-loader"
+				},
+				{
+					test: /\.html$/,
+					use: {
+						loader: 'html-loader',
+					}
 				}
-			}
-		]
-	},
-	mode: process.env.NODE_ENV || "development",
-	devServer: {
-		contentBase: __dirname + '/public',
-		publicPath: '/',
-		noInfo: true,
-		historyApiFallback: true
+			]
+		},
+		mode: (env && env.production) ? "production" : "development",
+		devServer: {
+			contentBase: __dirname + '/public',
+			publicPath: '/',
+			noInfo: true,
+			historyApiFallback: true
 
-	},
-	plugins: [
-		new webpack.BannerPlugin({
-			banner: `${packageJson.name} v${packageJson.version} | ${packageJson.author} | ${packageJson.license} license`
-		}),
-		new HTMLWebpackPlugin({
-			template: "./src/index.html"
-		}),
-		new HTMLWebpackPlugin({
-			template: "./src/style.css"
-		}),
-		new DtsBundlePlugin({
-			name: 'GeoLocoMap',
-			main: 'src/GeoLocoMap.d.ts',
-			baseDir: 'src',
-			out: `${__dirname}/dist/${packageJson.version}/@types/geo-loco-map.d.ts`
-		})
-	]
+		},
+		plugins: [
+			new webpack.BannerPlugin({
+				banner: `${packageJson.name} v${packageJson.version} | ${packageJson.author} | ${packageJson.license} license`
+			}),
+			new HTMLWebpackPlugin({
+				template: "./src/index.html"
+			}),
+			new HTMLWebpackPlugin({
+				template: "./src/style.css"
+			}),
+			new DtsBundlePlugin({
+				name: 'GeoLocoMap',
+				main: 'src/GeoLocoMap.d.ts',
+				baseDir: 'src',
+				out: `${__dirname}/dist/${packageJson.version}/@types/geo-loco-map.d.ts`
+			})
+		]
+	}
 };
