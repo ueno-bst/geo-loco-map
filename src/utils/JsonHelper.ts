14facc94a88c73e746f7280e7bcfa155b758ac6b
t-ueno <t.ueno.brook@gmail.com>
2020-01-22 11:55:09 +0900

    値の静的変換をサポート

--- src/utils/JsonHelper.ts
+++ src/utils/JsonHelper.ts
@@ -1,3 +1,9 @@
+import {isNull, isUndefined} from "./Types";
+
+const
+    is_undefined = isUndefined,
+    is_null = isNull;
+
 export class JsonHelper {
     private readonly data: any;
 
@@ -5,53 +11,77 @@ export class JsonHelper {
         this.data = data;
     }
 
-    convert(key: string, default_value: any, nullable: boolean = false): any {
-        if (this.data[key] === undefined) {
-            return nullable ? null : default_value;
+    static as<T = any>(value: T | undefined, def: T): T;
+    static as<T = any>(value: T | undefined, def: T, nullable: boolean): T | undefined;
+    static as<T = any>(value: T | undefined, def: T, nullable: boolean = false): T | undefined {
+        if (is_undefined(value)) {
+            return nullable ? undefined : def;
         }
-
-        return this.data[key];
+        return value;
     }
 
-    number(key: string, default_value: number, nullable: boolean = false): number {
-        let value = this.convert(key, default_value, nullable);
-
-        if (value === null) {
-            return value;
-        }
-
-        return Number(value);
+    static asNum(value: number | undefined, def: number): number;
+    static asNum(value: number | undefined, def: number, nullable: boolean): number | undefined;
+    static asNum(value: number | undefined, def: number, nullable: boolean = false): number | undefined {
+        const val = JsonHelper.as<number>(value, def, nullable);
+        return is_undefined(val) ? val : Number(val);
     }
 
-    string<T extends string>(key: string, default_value: string, nullable: boolean = false): string {
-        let value = this.convert(key, default_value, nullable);
-
-        if (value === null) {
-            return value;
-        }
-
-        return String(value);
+    static asStr(value: string | undefined, def: string): string;
+    static asStr(value: string | undefined, def: string, nullable: boolean): string | undefined;
+    static asStr(value: string | undefined, def: string, nullable: boolean = false): string | undefined {
+        const val = JsonHelper.as<string>(value, def, nullable);
+        return is_undefined(val) ? val : String(val);
     }
 
-    boolean(key: string, default_value: boolean, nullable: boolean = false): boolean {
-        let value = this.convert(key, default_value, nullable);
-
-        if (value === null) {
-            return value;
-        }
-
-        return Boolean(value);
+    static asBool(value: boolean | undefined, def: boolean): boolean;
+    static asBool(value: boolean | undefined, def: boolean, nullable: boolean): boolean | undefined;
+    static asBool(value: boolean | undefined, def: boolean, nullable: boolean = false): boolean | undefined {
+        const val = JsonHelper.as<boolean>(value, def, nullable);
+        return is_undefined(val) ? val : Boolean(val);
     }
 
-    enum(list: any, key: string, default_value: any, nullable: boolean = false): any {
-        let value = this.convert(key, default_value, nullable);
+    static asEnum<T>(list: { [key: string]: T }, value: T | undefined, def: T): T;
+    static asEnum<T>(list: { [key: string]: T }, value: T | undefined, def: T, nullable: boolean): T | undefined;
+    static asEnum<T>(list: { [key: string]: T }, value: T | undefined, def: T, nullable: boolean = false): T | undefined {
+        const val = JsonHelper.as<T>(value, def, nullable);
 
-        if (value === null) {
-            return value;
+        if (is_undefined(val)) {
+            return val;
         }
 
         let exists = Object.keys(list).filter(k => isNaN(Number(k))).filter(k => list[k] === value).length > 0;
 
-        return exists ? value : nullable ? null : default_value;
+        return exists ? val : nullable ? undefined : def;
+    }
+
+    get<T = any>(key: string, def: T): T;
+    get<T = any>(key: string, def: T, nullable: boolean): T | undefined;
+    get<T = any>(key: string, def: T, nullable: boolean = false): T | undefined {
+        return JsonHelper.as<T>(this.data[key], def, nullable);
+    }
+
+    number(key: string, def: number): number;
+    number(key: string, def: number, nullable: boolean): number | undefined;
+    number(key: string, def: number, nullable: boolean = false): number | undefined {
+        return JsonHelper.asNum(this.data[key], def, nullable);
+    }
+
+    string(key: string, def: string): string;
+    string(key: string, def: string): string | undefined
+    string(key: string, def: string, nullable: boolean = false): string | undefined {
+        return JsonHelper.asStr(this.data[key], def, nullable);
+    }
+
+    boolean(key: string, def: boolean): boolean;
+    boolean(key: string, def: boolean, nullable: boolean): boolean | undefined;
+    boolean(key: string, def: boolean, nullable: boolean = false): boolean | undefined {
+        return JsonHelper.asBool(this.data[key], def, nullable);
+    }
+
+    enum<T>(list: { [key: string]: T }, key: T, def: T): T;
+    enum<T>(list: { [key: string]: T }, key: T, def: T, nullable: boolean): T | undefined;
+    enum<T>(list: { [key: string]: T }, key: T, def: T, nullable: boolean = false): any | undefined {
+        return JsonHelper.asEnum<T>(list, this.data[key], def, nullable);
     }
 }
